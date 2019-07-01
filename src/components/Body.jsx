9b7b1bdd8a9b39c07f2b1924e89aeb8bb7d16f55
Reese Lee and Stuart McKay <reese+stuart@>
2019-07-01 16:52:19 -0700

    Rough in components using CSS grid

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,13 +5,37 @@ import Feed from './BodyComponents/Feed';
 import Connections from './BodyComponents/Connections';
 
 function Body() {
-
+    var Grid = {
+        display: "grid",
+        gridTemplateRows: "1fr 1fr",
+        gridTemplateColumns: "repeat(3, 1fr)"
+    }
+    var Column = {
+        gridColumnEnd: "span 1"
+    }
+    var ProfileStyle = {
+        GridColumn: "1 / 2",
+        GridRow: "1 / 2"
+    }
+    var BioStyle = {
+        GridColumn: "3 / 4",
+        GridRow: "2 / 3"
+    }
+    var FeedStyle = {
+        GridColumn: "2 / 3",
+        GridRow: "1 / 2"
+    }
+    var ConnectionsStyle = {
+        GridColumn: "1 / 2",
+        GridRow: "1 / 2"
+    }
+    
     return (
-        <div>
-            <Profile/>
-            <Bio/>
-            <Feed/>
-            <Connections/>
+        <div style={Grid}>
+            <Profile style={ProfileStyle}/>
+            <Feed style={FeedStyle}/>
+            <Connections style={ConnectionsStyle}/>
+            <Bio style={BioStyle}/>
         </div>
     );
 }
